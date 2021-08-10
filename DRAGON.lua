80b0fe9fccca0600ff0dc0348710269aae1a1e59
AHMED KHALED <82918788+Hiaveen@users.noreply.github.com>
2021-08-10 09:00:17 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -14197,7 +14197,7 @@ keyboard.inline_keyboard = {
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendvideo?chat_id=' .. msg.chat_id_ .. '&video=https://t.me/V_I_K_I_N_G_2/43/18&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-end🤔
+end
 if text == 'ضحك' then
 local Text = [[
 ]]
